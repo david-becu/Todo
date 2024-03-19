4af5b755f3e05401426de06a67dc5c2859205770
David Bécu <macnimo.db@free.fr>
2024-03-19 15:45:56 +0100

    First commit Todo test

---
+++ app.js
@@ -0,0 +1,19 @@
+import { fetchJSON } from "./functions/api.js" ;
+import { createElement } from "./functions/dom.js";
+import { TodoList } from "./components/TodoList.js"
+
+try {
+    
+    //console.log(todos)
+    const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    const list = new TodoList(todos)
+    list.appendTo(document.querySelector('#todolist'))
+
+} catch (e) {
+     const alertElement = createElement('div', {
+        class: 'alert alert-danger m-2',
+        role: 'alert'
+     })
+     alertElement.innerText = 'Impossible de charger les éléments'
+     document.body.prepend(alertElement) 
+}
---
+++ functions/dom.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {string} tagName 
+ * @param {object } attributtes
+ * @returns {HTMLElement} 
+ */
+export function createElement(tagName, attributtes = {}) {
+    const element = document.createElement(tagName)
+    
+    for (const [attribute, value] of Object.entries(attributtes)) {
+        if(value !== null) {
+            element.setAttribute(attribute, value)
+        }
+    }
+    return element
+}
