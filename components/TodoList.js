b548203d2f8d2ecf7ca2fa5bbb9e0ebb1a420caa
David Bécu <macnimo.db@free.fr>
2024-03-19 16:55:10 +0100

    appendTo() function modified for <template>

--- components/TodoList.js
+++ components/TodoList.js
@@ -26,23 +26,9 @@ export class TodoList {
      *  Pour indiquer à quel élément nous voulons ajouter une todoList
      */
     appendTo(element) {
-        element.innerHTML = `
-        <form class="d-flex pb-4">
-            <input required="" class="form-control" type="text" placeholder="Acheter des patates..." name="title"
-                data-com.bitwarden.browser.user-edited="yes">
-            <button class="btn btn-primary">Ajouter</button>
-        </form>
-        <main>
-            <div class="btn-group mb-4" role="group">
-                <button type="button" class=" btn btn-outline-primary active" data-filter="all">Toutes</button>
-                <button type="button" class=" btn btn-outline-primary" data-filter="todo">A faire</button>
-                <button type="button" class=" btn btn-outline-primary" data-filter="done">Faites</button>
-            </div>
-
-            <ul class="list-group">
-            </ul>
-        </main>
-`
+        element.append(
+            document.getElementById('todolist-layout').contentEditable.cloneNode(true)
+        )
         this.#listElement = element.querySelector('.list-group')
         for (let todo of this.#todos) {
             const t = new TodoListItem(todo)
